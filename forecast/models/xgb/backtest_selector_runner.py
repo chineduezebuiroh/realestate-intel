81a1a075c2af0d889e7bb18fcc31c0135f686e6d
chineduezebuiroh <c.eze29@gmail.com>
2026-01-25 14:22:57 -0500

    Add import for RedfinTierShareCaps
    Import RedfinTierShareCaps from forecast.metric_tiers.

--- forecast/models/xgb/backtest_selector_runner.py
+++ forecast/models/xgb/backtest_selector_runner.py
@@ -32,6 +32,8 @@ from forecast.feature_selection import (
     scored_to_feature_specs,
     default_bucket,
 )
+from forecast.metric_tiers import RedfinTierShareCaps
+
 
 TEMP_DEBUG_LIMIT = None  # set to an int for debugging; None for normal operation
 
