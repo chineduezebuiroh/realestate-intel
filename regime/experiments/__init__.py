d31620aa6a4d25d6f75b0f910c5385e359eb1b8a
chineduezebuiroh <c.eze29@gmail.com>
2026-07-13 00:17:56 -0400

    Initialize __init__.py for experiments module

---
+++ regime/experiments/__init__.py
@@ -0,0 +1,13 @@
+from regime.experiments.smoothing_policy import (
+    DEFAULT_SMOOTHING_EXPERIMENT_REGISTRY,
+    SmoothingExperiment,
+    SmoothingMetricPolicy,
+    load_smoothing_experiments,
+)
+
+__all__ = [
+    "DEFAULT_SMOOTHING_EXPERIMENT_REGISTRY",
+    "SmoothingExperiment",
+    "SmoothingMetricPolicy",
+    "load_smoothing_experiments",
+]
