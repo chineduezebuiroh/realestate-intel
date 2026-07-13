b3be25ddf3458780b3aec933d156cede32d70165
chineduezebuiroh <c.eze29@gmail.com>
2026-07-13 01:21:12 -0400

    Import smoothing features in __init__.py

--- regime/experiments/__init__.py
+++ regime/experiments/__init__.py
@@ -1,3 +1,7 @@
+from regime.experiments.smoothing_features import (
+    build_smoothed_metric_features,
+    build_smoothed_metric_features_wide,
+)
 from regime.experiments.smoothing_policy import (
     DEFAULT_SMOOTHING_EXPERIMENT_REGISTRY,
     SmoothingExperiment,
@@ -9,5 +13,7 @@ __all__ = [
     "DEFAULT_SMOOTHING_EXPERIMENT_REGISTRY",
     "SmoothingExperiment",
     "SmoothingMetricPolicy",
+    "build_smoothed_metric_features",
+    "build_smoothed_metric_features_wide",
     "load_smoothing_experiments",
 ]
