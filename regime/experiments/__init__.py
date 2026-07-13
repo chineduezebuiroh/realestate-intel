d5536965c155431620ae37d4dc4d3576f63b9c46
chineduezebuiroh <c.eze29@gmail.com>
2026-07-13 05:07:39 -0400

    Add build_active_inventory_comparison to __all__

--- regime/experiments/__init__.py
+++ regime/experiments/__init__.py
@@ -8,6 +8,9 @@ from regime.experiments.smoothing_policy import (
     SmoothingMetricPolicy,
     load_smoothing_experiments,
 )
+from regime.experiments.active_inventory_comparison import (
+    build_active_inventory_comparison,
+)
 
 __all__ = [
     "DEFAULT_SMOOTHING_EXPERIMENT_REGISTRY",
@@ -16,4 +19,5 @@ __all__ = [
     "build_smoothed_metric_features",
     "build_smoothed_metric_features_wide",
     "load_smoothing_experiments",
+    "build_active_inventory_comparison",
 ]
